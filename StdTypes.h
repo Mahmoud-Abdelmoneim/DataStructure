92fe7e377874b3f208eda89ef3ef335edcfcc431
Mahmoud Abdelmoniem <109484204+Mahmoud-Abdelmoneim@users.noreply.github.com>
2023-08-01 12:03:51 +0300

    Add files via upload

---
+++ StdTypes.h
@@ -0,0 +1,38 @@
+/**
+  ******************************************************************************
+  * @auther         : Mahmoud Abdelmoniem
+  * @data           : 19/9/2022
+  * @file           : Stack.c
+  * @brief          : Stack program body
+  ******************************************************************************
+  */
+#ifndef  __STDTYPES_H__
+#define __STDTYPES_H__
+
+
+#define ZERO_INIT      0
+#define ZERO           0
+
+
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+
+
+typedef enum {
+
+	R_NOK,
+	R_OK
+}Return_Status_t;
+
+
+
+
+
+#endif // ! __STDTYPES__
+
